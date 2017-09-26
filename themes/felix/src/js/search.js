27253ddd90bb88bb446a244ee28af01efe5b7af2
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-09-26 20:45:59 +0100

    Add homepage search

--- themes/felix/src/js/search.js
+++ themes/felix/src/js/search.js
@@ -1,45 +1,64 @@
 if (document.location.pathname.includes("search")) {
-  function buildArticleResult(article) {
-    var result = document.createElement("div");
+  var search = {
+    buildArticleResult: function(article) {
+      var result = document.createElement("div");
 
-    var link = document.createElement("a");
-    link.href = article.link;
-    link.innerHTML = article.title;
-    link.setAttribute("target", "_blank")
+      var link = document.createElement("a");
+      link.href = article.link;
+      link.innerHTML = article.title;
+      link.setAttribute("target", "_blank")
 
-    var image = document.createElement("img");
-    image.setAttribute("src", "//images.weserv.nl/?url=" + article.image.replace("https://", "") + "&w=100");
+      var image = document.createElement("img");
+      image.setAttribute("src", "//images.weserv.nl/?url=" + article.image.replace("https://", "") + "&w=100");
 
-    var subtitle = document.createElement("p");
-    subtitle.innerHTML = article.subtitle;
+      var subtitle = document.createElement("p");
+      subtitle.innerHTML = article.subtitle;
 
-    result.appendChild(image);
-    result.appendChild(link);
-    result.appendChild(subtitle);
+      result.appendChild(image);
+      result.appendChild(link);
+      result.appendChild(subtitle);
 
-    return result;
-  }
+      return result;
+    },
 
-  ajax().get('/v2/site_index/index.json').then(function (articles, xhr) {
-    var searchInput = document.getElementById("search");
-    var searchResults = document.getElementById("results");
+    getURLParameter: function(name) {
+      return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+    },
 
-    searchInput.oninput = function() {
-      searchResults.innerHTML = "";
-      if (searchInput.value.length < 3) { return }
+    perform: function() {
+      search.resultsList.innerHTML = "";
+      if (search.inputBox.value.length < 3) { return }
 
       var results = [];
-      for (var i in articles) {
-        if (articles[i].title.toLowerCase().includes(searchInput.value.toLowerCase())) {
-          results.push(articles[i]);
+      for (var i in search.articles) {
+        if (search.articles[i].title.toLowerCase().includes(search.inputBox.value.toLowerCase())) {
+          results.push(search.articles[i]);
           if (results.length > 5) {
             break;
           }
         }
       }
       for (var i in results) {
-        searchResults.appendChild(buildArticleResult(results[i]));
+        search.resultsList.appendChild(search.buildArticleResult(results[i]));
       }
+    },
+
+    init: function() {
+      ajax().get('/v2/site_index/index.json').then(function (data, xhr) {
+        search.articles = data;
+        search.inputBox = document.getElementById("search");
+        search.resultsList = document.getElementById("results");
+
+        search.inputBox.oninput = search.perform;
+
+        var queryParam = search.getURLParameter("q");
+        if (queryParam != null) {
+          search.inputBox.value = queryParam;
+          search.perform();
+        }
+      })
     }
-  });
+  }
+
+  search.init();
 }
