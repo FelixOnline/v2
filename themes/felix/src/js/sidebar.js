0f3de650a508755d289373461b2bbd64d65cc4db
Cale Tilford <combinekidd@gmail.com>
2017-10-06 21:58:28 +0100

    Style sidebar nav and search

--- themes/felix/src/js/sidebar.js
+++ themes/felix/src/js/sidebar.js
@@ -1,9 +1,9 @@
 function openNav() {
   document.getElementById("sidenav").style.width = "100%";
-  // document.getElementById("site-body").style.marginLeft = "250px";
+  document.getElementById("site-body").classList.add("site-blur");
 }
 
 function closeNav() {
   document.getElementById("sidenav").style.width = "0";
-  // document.getElementById("site-body").style.marginLeft = "0";
+  document.getElementById("site-body").classList.remove("site-blur");
 }
